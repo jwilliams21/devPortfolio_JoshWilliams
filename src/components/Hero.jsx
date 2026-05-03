594fcee841fffb3d2287ba9b4f0fbd49dd1656fc
Josh Williams <joshbenjaminwilliams@gmail.com>
2026-05-03 07:56:29 -0400

    Hero.jsx Update
    Update spacing and formatting of Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,14 +5,14 @@ export default function Hero() {
 
     return (
         <section id="hero" 
-        className="bg-linear-to-br from-slate-900 via-slate-800 to-slate-900 min-h-screen text-gray-200 flex flex-col items-center justify-center m-0 w-full overflow-hidden scroll-mt-20 pt-4">
-            <span>Welcome to my portfolios</span>
-            <h1 className="text-4xl md:text-6xl py-8 bg-linear-to-r from-white to-gray-400 bg-clip-text text-transparent font-bold px-4 text-center wrap-break-word max-w-full leading-tight">
+        className="bg-linear-to-br from-slate-900 via-slate-800 to-slate-900 min-h-screen text-gray-200 flex flex-col items-center justify-center m-0 w-full overflow-hidden scroll-mt-20">
+            <span>Welcome to my portfolio</span>
+            <h1 className="tracking-tight text-4xl md:text-6xl py-8 bg-linear-to-r from-white to-gray-400 bg-clip-text text-transparent font-bold px-4 text-center wrap-break-word max-w-full leading-tight">
                 Front-End Developer & Technical Leader
             </h1>
-            <div className="max-w-3xl px-4 pb-12">
+            <div className="max-w-2xl px-4 pb-12">
                 <h2 className="text-2xl md:text-4xl font-semibold text-center wrap-break-word leading-tight">
-                    Specializing in building responsive, user-centric applications with React and Tailwind CSS. Leveraging years of SaaS leadership to deliver high-quality code and strategic solutions.
+                    Building user-centric React applications with a focus on scalable code and strategic technical leadership.
                 </h2>
             </div>
             <div className="text-base md:text-lg flex flex-col md:flex-row gap-4 md:gap-8 px-4">
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,14 +1,26 @@
 
-export default function Button({ children, onClick, variant, className = "", type = "button" }) {
+export default function Button({ children, onClick, variant, className = "", href }) {
     const variants = {
         hero: "border-slate-400 border-solid border p-4 rounded-lg  hover:text-slate-900 hover:bg-gray-200 hover:border-slate-900 hover:font-semibold"
     }
 
+    const combinedClasses = `${variants[variant]} ${className}`;
+
+    if (href) {
+        return (
+            <a
+                href={href} 
+                className={`${combinedClasses}`}
+            >
+                {children}
+            </a>
+        );
+    }
+
     return (
         <button
-            type={type}
             onClick={onClick}
-            className={`${variants[variant]} ${className}`}
+            className={`${combinedClasses}`}
         >
             {children}
         </button>
